78d4857aeb1247d4ded9acb6365ecf5f27b9c80a
blurfrost <ksyc07@gmail.com>
2025-10-16 16:03:52 +0800

    Update link to user guide

--- src/main/java/seedu/address/ui/HelpWindow.java
+++ src/main/java/seedu/address/ui/HelpWindow.java
@@ -19,7 +19,7 @@ import seedu.address.commons.core.LogsCenter;
  */
 public class HelpWindow extends UiPart<Stage> {
 
-    public static final String USERGUIDE_URL = "https://se-education.org/addressbook-level3/UserGuide.html";
+    public static final String USERGUIDE_URL = "https://ay2526s1-cs2103t-w14-3.github.io/tp/UserGuide.html";
     public static final String HELP_MESSAGE = "Refer to the user guide: " + USERGUIDE_URL;
     public static final String SUPPORTED_COMMANDS_HEADER_TEXT = "List of supported commands:";
 
