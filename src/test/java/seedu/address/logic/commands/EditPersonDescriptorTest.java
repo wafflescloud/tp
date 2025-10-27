2581b2729583d10f82146f1a4760dc1a35534b15
blurfrost <ksyc07@gmail.com>
2025-10-27 11:07:13 +0800

    Remove feed functionality for edit

--- src/test/java/seedu/address/logic/commands/EditPersonDescriptorTest.java
+++ src/test/java/seedu/address/logic/commands/EditPersonDescriptorTest.java
@@ -59,8 +59,7 @@ public class EditPersonDescriptorTest {
                 + editPersonDescriptor.getName().orElse(null) + ", phone="
                 + editPersonDescriptor.getPhone().orElse(null) + ", email="
                 + editPersonDescriptor.getEmail().orElse(null) + ", tags="
-                + editPersonDescriptor.getTags().orElse(null) + ", intermediateFeedingSessions="
-                + editPersonDescriptor.getIntermediateFeedingSessions().orElse(null) + "}";
+                + editPersonDescriptor.getTags().orElse(null) + "}";
         assertEquals(expected, editPersonDescriptor.toString());
     }
 }
