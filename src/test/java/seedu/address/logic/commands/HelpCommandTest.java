9ef3dba17ad6150dc49e994d544d58922678bc50
blurfrost <ksyc07@gmail.com>
2025-10-29 00:18:06 +0800

    Add more tests for HelpCommand

--- src/test/java/seedu/address/logic/commands/HelpCommandTest.java
+++ src/test/java/seedu/address/logic/commands/HelpCommandTest.java
@@ -17,4 +17,41 @@ public class HelpCommandTest {
         CommandResult expectedCommandResult = new CommandResult(SHOWING_HELP_MESSAGE, true, false);
         assertCommandSuccess(new HelpCommand(), model, expectedCommandResult, expectedModel);
     }
+
+    @Test
+    public void execute_helpForSpecificCommand_success() {
+        String commandName = "add";
+        CommandResult expectedCommandResult = new CommandResult(
+                "Showing help for " + commandName, false, false, commandName);
+        assertCommandSuccess(new HelpCommand(commandName), model, expectedCommandResult, expectedModel);
+
+        commandName = "delete";
+        expectedCommandResult = new CommandResult(
+                "Showing help for " + commandName, false, false, commandName);
+        assertCommandSuccess(new HelpCommand(commandName), model, expectedCommandResult, expectedModel);
+
+        commandName = "list";
+        expectedCommandResult = new CommandResult(
+                "Showing help for " + commandName, false, false, commandName);
+        assertCommandSuccess(new HelpCommand(commandName), model, expectedCommandResult, expectedModel);
+
+        commandName = "help";
+        expectedCommandResult = new CommandResult(
+                "Showing help for " + commandName, false, false, commandName);
+        assertCommandSuccess(new HelpCommand(commandName), model, expectedCommandResult, expectedModel);
+    }
+
+    @Test
+    public void execute_helpForUnknownCommand_unrecognized() {
+        CommandResult expectedCommandResult = new CommandResult(
+                HelpCommand.UNRECOGNIZED_COMMAND_MESSAGE, true, false);
+        assertCommandSuccess(new HelpCommand("unknown", true), model, expectedCommandResult, expectedModel);
+    }
+
+    @Test
+    public void execute_helpForEmptyCommand_unrecognized() {
+        CommandResult expectedCommandResult = new CommandResult(
+                HelpCommand.UNRECOGNIZED_COMMAND_MESSAGE, true, false);
+        assertCommandSuccess(new HelpCommand(" ", true), model, expectedCommandResult, expectedModel);
+    }
 }
