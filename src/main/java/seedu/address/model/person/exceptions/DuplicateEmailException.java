eb7bc841acc2392d351fcd758f8dbfc782b2a1b8
jlismee <e1355121@u.nus.edu>
2025-10-29 22:21:09 +0800

    Fix checkstyle errors

--- src/main/java/seedu/address/model/person/exceptions/DuplicateEmailException.java
+++ src/main/java/seedu/address/model/person/exceptions/DuplicateEmailException.java
@@ -4,7 +4,7 @@ package seedu.address.model.person.exceptions;
  * Signals that the operation will result in duplicate Emails
  */
 public class DuplicateEmailException extends RuntimeException {
-  public DuplicateEmailException() {
-    super("Operation would result in duplicate emails");
-  }
+    public DuplicateEmailException() {
+        super("Operation would result in duplicate emails");
+    }
 }
