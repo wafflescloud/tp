a1b12b95e499dd369bf733d59fdb96f879db41a5
wafflescloud <e1354308@u.nus.edu>
2025-11-03 23:55:58 +0800

    Fix styling error

--- src/test/java/seedu/address/logic/commands/RedoCommandTest.java
+++ src/test/java/seedu/address/logic/commands/RedoCommandTest.java
@@ -11,8 +11,7 @@ import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
-import seedu.address.model.ModelManager;
-import seedu.address.model.UserPrefs;
+import seedu.address.model.ModelManager;g
 import seedu.address.model.person.Person;
 import seedu.address.testutil.PersonBuilder;
 
