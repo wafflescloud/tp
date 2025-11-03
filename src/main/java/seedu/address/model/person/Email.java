00eb199ea00d846ec0ae0211657286d926369d0a
morpheuschoo <morpheuschoo@gmail.com>
2025-11-03 20:15:15 +0800

    Fix minor bugs

--- src/main/java/seedu/address/model/person/Email.java
+++ src/main/java/seedu/address/model/person/Email.java
@@ -42,7 +42,7 @@ public class Email {
      * Returns if a given string is a valid email.
      */
     public static boolean isValidEmail(String test) {
-        return test.length() <= 998 && test.matches(VALIDATION_REGEX);
+        return test.length() <= 998 && test.toLowerCase().matches(VALIDATION_REGEX);
     }
 
     @Override
